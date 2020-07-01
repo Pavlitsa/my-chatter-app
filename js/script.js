afee516e1ded6c2c5938100af586cbd851befb55
Pavlitsa <pzafeiri@gmail.com>
2020-07-01 12:48:57 +0200

    js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,29 @@ function switchChannel(channelName, location) {
     $('#chat-name').html(channelName + ' by');
     $('#title-name').html(location);
     $('#title-name').attr("href", "https://what3words.com/reeling.rainbow.martini");
+    $('.selected-list-item').removeClass('selected-list-item');
+    $('.channel-item:contains('+ channelName +')').addClass('selected-list-item');
     console.log('Tuning into channel ' + channelName + " by " + location);
 }
 
-// function changeTime(time) {
-//     $('.rmn-time').html(time);
+function channelChangeStar() {
+    $('#bar-full-star').attr('src','https://ip.lfe.mw.tum.de/sections/star-o.png');
+}
+
+function fillChannelStar() {
+    $('#bar-full-star').attr('src','https://ip.lfe.mw.tum.de/sections/star.png');
+}
+
+// function removeTab() {
+//     $('.tab-bar > button').removeClass("selected");
 // }
+
+function addTab(tabId) {
+    $('.selected').removeClass('selected');
+    $(tabId).addClass('selected'); 
+}
+
+
+function showEmojis() {
+    $('#emojis').toggle();
+}
